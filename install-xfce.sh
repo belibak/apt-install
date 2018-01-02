7d6ea0f92afbe49028bc39cdb3de9065d2f7f817
belibak <belibak85@gmail.com>
2018-01-02 19:02:46 +0300

    xfce

---
+++ install-xfce.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+apt update
+apt install -y \
+cifs-utils \
+net-tools \
+tmux \
+netcat \
+vim \
+sudo \
+htop \
+lm-sensors \
+python3 \
+python3-pip \
+python3-dev \
+python-virtualenv \
+wireshark \
+nmap \
+shutter \
+xneur \
+xfce4-whiskermenu-plugin \
+audacious \
+vlc \
+gnome-calculator \
+thunderbird \
+gvfs-backends \
+gedit
